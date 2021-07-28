020bae947e412dfa8af3deb568b81f8eee6ec86d
Roberto919 <robertosysm@gmail.com>
2021-07-27 19:38:45 -0500

    almost completing methods api params

--- src/utils/isabel_params.py
+++ src/utils/isabel_params.py
@@ -43,12 +43,60 @@ feed_loc = "../feed/"
 
 isabel_api_params = {
     "base_url": "https://apiscsandbox.isabelhealthcare.com/v2/",
-    "age_groups": {
-        "params": [
-            "language",
-            "web_service"
-        ]
-    }
+    "methods": {
+
+        "age_groups": {
+            "params_required": [
+                "language",
+                "web_service"
+            ],
+            "params_input": feed_loc + "age_groups_input.json"
+        },
+
+        "regions": {
+            "params_required": [
+                "language",
+                "web_service"
+            ],
+            "params_input": feed_loc + "regions_input.json"
+        },
+
+        "countries": {
+            "params_required": [
+                "language",
+                "web_service"
+            ],
+            "params_input": feed_loc + "countries_input.json"
+        },
+
+        "pregnancies": {
+            "params_required": [
+                "language",
+                "web_service"
+            ],
+            "params_input": feed_loc + "pregnancies_input.json"
+        },
+
+        "ranked_differential_diagnoses": {
+            "params_required": [
+                "specialties", ## 28 for rdd
+                "dob", ## format: YYMMDD
+                "sex", ## "m" (male) or "f" (female)
+                "pregnant", ## "n" (no), "y" (pregnant), "blank" (not specified)
+                "region", ## there is a reference table; this parameter is only used when `country_id` is not passed
+                "country_id", ## api (method "countries") is the reference
+                "querytext", ## symptoms comma separated “,” E.g. – fever,cold,nausea
+                "flag", ## Pass “red_flag” for don't miss diagnoses or “sortbyRW_advanced” for most relevant diagnoses (ranked list of diagnoses)
+                "suggest", ## Suggest+Differential+Diagnosis (fixed value)
+                "searchType",
+                "web_service",
+                # "callback",
+                # "authorization",
+            ],
+            "params_input": feed_loc + "pregnancies_input.json"
+        },
+
+    },
 }
 
 
