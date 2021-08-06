5be2c946b6b1b1ee0d8662e123da671b1544a798
Roberto919 <robertosysm@gmail.com>
2021-08-06 18:40:28 -0500

    completing main function to request information for menus

--- isabel_menus_main.py
+++ isabel_menus_main.py
@@ -9,6 +9,26 @@
 ## Imports ##
 #############
 
+## Standard library imports
+import requests
+
+
+## Third party imports
+
+
+## Local application imports
+from src.utils.isabel_funcs import (
+    url_api_request,
+    enhance_api_response,
+    write_json,
+)
+
+from src.utils.isabel_params import (
+    inout_files,
+    out_loc,
+    isabel_api_params,
+)
+
 
 
 
@@ -18,6 +38,39 @@
 ## Main function ##
 ###################
 
+## Main function to populate all menus in user interface
+def main_func_pop_menus():
+    """
+    Main function to populate all menus in user interface
+
+    :return none:
+    """
+
+    ## Executing all methods with data for the GUI
+
+    #### List of methods relevant to the GUI
+    methods = [mthd for mthd in isabel_api_params["methods"] if isabel_api_params["methods"][mthd]["usage"] == "GUI"]
+
+    #### Loop to update the output files of every selected method
+    for method in methods:
+
+        ###### Building the URL required for the API requests
+        api_call_url = url_api_request(method)
+
+        ###### Sending the request to the API
+        req = requests.get(api_call_url)
+
+        ###### Formatting and enhancing the response
+        api_resp = enhance_api_response(req.text)
+
+        ###### Writing result in output file
+        path = out_loc + inout_files[method]["output"]
+        write_json(path, req.text)
+
+    return
+
+
+
 
 
 
@@ -26,6 +79,10 @@
 ################################
 ## Execution of main function ##
 ################################
+
+## Automate execution of main function
+if __name__ == "__main__":
+    main_func_pop_menus()
 
 
 
